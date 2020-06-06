fc8aa1b278e2b334ad0b51a1a808f34a04b7e95b
BenjaminFern <66424102+BenjaminFern@users.noreply.github.com>
2020-06-06 14:09:14 -0600

    Updated 6/6 2:09pm

--- Hackathon.cpp
+++ Hackathon.cpp
@@ -14,34 +14,37 @@ int main()
     int i = 0;
     int j = 0;
     bool escape = false;
+    bool fileSkip = false;
 
     // loop for Task Manager
 
     ofstream file;
-    file.open ("Problem.txt"); // NAME CAN CHANGE
+    file.open ("TaskList.txt"); // NAME CAN CHANGE
     file.close ();
+    
+    // system(("tasklist > " + directory variable + "\\TaskList.txt").c_str());
 
-    system("tasklist > C:\\Users\\benja\\OneDrive\\Documents\\Hackathon\\Problem.txt"); //*****EDIT THIS*****
+    system("tasklist > C:\\Users\\benja\\OneDrive\\Documents\\Hackathon\\TaskList.txt"); //*****EDIT THIS W/ Directory Location*****
 
     fstream files;
     string word;
     string filename;
     string type[10];
     type[0] = "Nothing"; // PLACE HOLDER
-    filename = "Problem.txt";
+    filename = "TaskList.txt";
     
     
     while (!exit)
     {
+        i = 0;
+        type[0] = "Nothing";
         while (type[0] == "Nothing")
         {
-            system("tasklist > C:\\Users\\benja\\OneDrive\\Documents\\Hackathon\\Problem.txt"); //*****EDIT THIS ******
+            system("tasklist > C:\\Users\\benja\\OneDrive\\Documents\\Hackathon\\TaskList.txt"); //*****EDIT THIS ******
             files.open(filename);
 
             while (files >> word)
             {
-
-                cout << word << endl; // DELETE THIS LINE
                 //This will search for the program to open
                 if (word == "WINWORD.EXE" || word == "POWERPNT.EXE" || word == "AcroRd32.exe")
                 {
@@ -58,13 +61,17 @@ int main()
             }
             files.close();
         }
+        // Reset i variable value
+        i = 0;
+
         //Run vector list for name ***************************
 
         cout << "Would you like to save or delete this file? (s/d) ";
         cin >> save;
+        fileSkip = false;
         if (save == 's' || save == 'S')
         {
-            exit = true;
+            fileSkip = true;
             // then change vector to saved **************
         }
         else
@@ -74,20 +81,21 @@ int main()
         }
 
         // task manager PID close
-        if (!exit)
+        if (!fileSkip)
         {
+            escape = false;
             while (escape == false)
             {
-                system("tasklist > C:\\Users\\benja\\OneDrive\\Documents\\Hackathon\\Problem.txt"); // COULD CHANGE FIX
+                system("tasklist > C:\\Users\\benja\\OneDrive\\Documents\\Hackathon\\TaskList.txt"); // COULD CHANGE FIX
                 files.open(filename);
-                escape == true;
+                escape = true;
                 while (files >> word)
                 {
 
                     //This will search to see if the program is still open
                     if (word == type[i])
                     {
-                        escape == false;
+                        escape = false;
 
                     }
                     else
@@ -103,24 +111,26 @@ int main()
 
             //system("del /f DELETE.txt"); INSERT THE DIRECTORY NAME AND FILE NAME HERE TO DELETE ****************
 
-            // prompt exit program
-
-            cout << "Would you like to exit this program? (y/n) ";
-            cin >> exitChar;
-
-            if (exitChar == 'y' || exitChar == 'Y')
-            {
-            exit = true;
-            }
-            else
-            {
-                exit = false;
-            }
+            
         }
         else
         {
             ;
         }
+
+        // prompt exit program
+        cout << "Would you like to exit this program? (y/n) ";
+        cin >> exitChar;
+
+        if (exitChar == 'y' || exitChar == 'Y')
+        {
+            exit = true;
+        }
+        else
+        {
+            exit = false;
+        }
+
     }
 
     cout << "Thank you for using Incognito Files!";
