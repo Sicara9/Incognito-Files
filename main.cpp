43fa9955393a1ea91ba48d13123f8445c547d738
Sicara9 <briankoh12@gmail.com>
2020-06-05 22:39:36 -0700

    Merge branch 'master' of https://github.com/Sicara9/Hackathon-2020

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <fstream>
 #include <string>
 
-#include "database.cpp"
-
+//#include "database.cpp"
+#include "directory.cpp"
 using namespace std;
-
 
 int main() {
 
+//  int looper = 1;
+  ofstream fOut;
+  ifstream dir;
+  string iDir;
+  string tempDir;
+
+  parseDirectory(dir, fOut, tempDir, iDir);
+  
 
   return 0;
 }
