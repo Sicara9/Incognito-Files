9a0d797bf65cf59e51f7c7e1794e202d8aba9eac
Sicara9 <briankoh12@gmail.com>
2020-06-06 21:37:15 -0700

    Update setup.cpp

--- setup.cpp
+++ setup.cpp
@@ -21,14 +21,21 @@ void parseDirectory (ifstream &fin_D, ofstream &fileOut, string &tempDirectory, 
           if (S_ISDIR(stats.st_mode)) {
             fileOut.open("database.txt");
             fileOut.close();
-            system(("dir " + tempDirectory + " > database.txt").c_str());
+            system(("dir \"" + tempDirectory + "\" > database.txt").c_str());
           }
           else {
             createDirectory (fileOut, input_directory);
           }
         }
+        else {
+          fin_D.close();
+          createDirectory (fileOut, input_directory);
+        }
       }
-      fin_D.close();
+      else {
+        fin_D.close();
+        createDirectory (fileOut, input_directory);
+      }
     }
     else {
       createDirectory (fileOut, input_directory);
@@ -44,7 +51,7 @@ void createDirectory (ofstream &fOut_D, string &input_directory) {
   fOut_D.open("directory.txt");
   fOut_D << "File: " << endl;
   cout << "Input a directory path that you would like your files to be monitored for deletion: ";
-  cin >> input_directory;
+  getline(cin, input_directory);
   fOut_D << input_directory;
   cout << endl << input_directory << endl;
   fOut_D.close();
@@ -69,19 +76,16 @@ void addFileType (ifstream &fin_FT, ofstream &fOut_FT, vector<string> &fileTypes
       fileTypes.push_back(end);
       getline (fin_FT, end);
     }
-
     fin_FT.close();
   }
 
   fOut_FT.open("FileTypes.txt");
-
-  if (fileTypes.size() > 0) {
-    for (auto i = fileTypes.begin(); i > fileTypes.end(); i++) {
-      fOut_FT << *i << "\n";
-    }
+  for (auto i = fileTypes.begin(); i != fileTypes.end(); i++) {
+    cout << *i << "\n";
   }
-  else {
-    ;
+cout << "hello" << endl;
+  for (auto i = fileTypes.begin(); i != fileTypes.end(); i++) {
+    fOut_FT << *i << "\n";
   }
 
   cout << "Enter in the file types you want to look out for." << endl
@@ -92,8 +96,8 @@ void addFileType (ifstream &fin_FT, ofstream &fOut_FT, vector<string> &fileTypes
 
   cin >> tempFileType;
 
-  string fileSyn[] = {".docx", ".pdf", ".ppt", ".png", ".gif"}; // Actually insert all file types here....
-  int i = 0;
+  vector<string> fileSyn{".docx", ".pdf", ".ppt", ".png", ".gif"}; // Actually insert all file types here....
+  //int i = 0;
   fileTypes.clear();
 
   while (tempFileType != "*") {
@@ -101,9 +105,9 @@ void addFileType (ifstream &fin_FT, ofstream &fOut_FT, vector<string> &fileTypes
       fileTypes.clear();
       fOut_FT.close();
       fOut_FT.open("FileTypes.txt");
-      for (i = 0; fileSyn; i++) {
-        fOut_FT << fileSyn[i] << "\n";
-        fileTypes.push_back(fileSyn[i]);
+      for (auto i = fileSyn.begin(); i != fileSyn.end(); i++) {
+        fOut_FT << *i << "\n";
+        fileTypes.push_back(*i);
       }
       cin >> tempFileType;
     }
@@ -114,148 +118,15 @@ void addFileType (ifstream &fin_FT, ofstream &fOut_FT, vector<string> &fileTypes
       cin >> tempFileType;
      }
     else if (tempFileType == "filetypes") {
-      if (fileTypes.size() > 0) {
-        for (auto i = fileTypes.begin(); i > fileTypes.end(); i++) {
-        cout << *i << "\n";
+      if (fileTypes.begin() == fileTypes.end()) {
+        for (auto i = fileTypes.begin(); i != fileTypes.end(); i++) {
+        cout << *i << endl;
         }
+        cin >> tempFileType;
       }
       else {
         cout << "There are no file types currently selected.";
-      }
-    }
-    else {
-    fileTypes.push_back(tempFileType);
-    fOut_FT << tempFileType << "\n";
-    cin >> tempFileType;
-    }
-  }
-  fOut_FT << tempFileType;
-
-  fOut_FT.close();
-}
-#include <iostream>
-#include <fstream>
-#include <sys/stat.h>
-
-#include "setup.h"
-
-using namespace std;
-
-void parseDirectory (ifstream &fin_D, ofstream &fileOut, string &tempDirectory, string &input_directory) {
-  struct stat stats;
-  fin_D.open("directory.txt");
-
-  if (fin_D.is_open()) {
-
-    if (getline (fin_D, tempDirectory)) {
-      if (tempDirectory == "File: ") {
-        if (getline (fin_D, tempDirectory)) {
-          // if (!system(("dir " + tempDirectory).c_str())) {
-          // const string tempDir = tempDirectory;
-          stat(tempDirectory.c_str(), &stats);
-          if (S_ISDIR(stats.st_mode)) {
-            fileOut.open("database.txt");
-            fileOut.close();
-            system(("dir " + tempDirectory + " > database.txt").c_str());
-          }
-          else {
-            createDirectory (fileOut, input_directory);
-          }
-        }
-      }
-      fin_D.close();
-    }
-    else {
-      createDirectory (fileOut, input_directory);
-    }
-  }
-  else {
-    fin_D.close();
-    createDirectory (fileOut, input_directory);
-  }
-}
-
-void createDirectory (ofstream &fOut_D, string &input_directory) {
-  fOut_D.open("directory.txt");
-  fOut_D << "File: " << endl;
-  cout << "Input a directory path that you would like your files to be monitored for deletion: ";
-  cin >> input_directory;
-  fOut_D << input_directory;
-  cout << endl << input_directory << endl;
-  fOut_D.close();
-}
-
-void addFileType (ifstream &fin_FT, ofstream &fOut_FT, vector<string> &fileTypes) {
-
-  string tempFileType;
-  string end;
-
-  fileTypes.clear();
-  fin_FT.open("FileTypes.txt");
-
-  if (!fin_FT.is_open()) {
-    fOut_FT.open("FileTypes.txt");
-    fOut_FT.close();
-  }
-  else {
-    getline (fin_FT, end);
-
-    while (end != "*") {
-      fileTypes.push_back(end);
-      getline (fin_FT, end);
-    }
-
-    fin_FT.close();
-  }
-
-  fOut_FT.open("FileTypes.txt");
-
-  if (fileTypes.size() > 0) {
-    for (auto i = fileTypes.begin(); i > fileTypes.end(); i++) {
-      fOut_FT << *i << "\n";
-    }
-  }
-  else {
-    ;
-  }
-
-  cout << "Enter in the file types you want to look out for." << endl
-  << "- Type 'all' to look for all file types." << endl
-  << "- Type 'clear' to remove all file types you are currently looking for. " << endl
-  << "- Type 'filetypes' to view all files you are currently looking for." << endl
-  << "- Type * when done: ";
-
-  cin >> tempFileType;
-
-  string fileSyn[] = {".docx", ".pdf", ".ppt", ".png", ".gif"}; // Actually insert all file types here....
-  int i = 0;
-  fileTypes.clear();
-
-  while (tempFileType != "*") {
-    if (tempFileType == "all") {
-      fileTypes.clear();
-      fOut_FT.close();
-      fOut_FT.open("FileTypes.txt");
-      for (i = 0; fileSyn; i++) {
-        fOut_FT << fileSyn[i] << "\n";
-        fileTypes.push_back(fileSyn[i]);
-      }
-      cin >> tempFileType;
-    }
-    else if (tempFileType == "clear") {
-      fileTypes.clear();
-      fOut_FT.close();
-      fOut_FT.open("FileTypes.txt");
-      cin >> tempFileType;
-     }
-    else if (tempFileType == "filetypes") {
-      if (fileTypes.size() > 0) {
-        for (auto i = fileTypes.begin(); i > fileTypes.end(); i++) {
-        cout << *i << "\n";
-        }
-      }
-      else {
-        cout << "There are no file types currently selected.";
+        cin >> tempFileType;
       }
     }
     else {
